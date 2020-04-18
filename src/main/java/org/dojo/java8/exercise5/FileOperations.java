75db00f0fe7b22581a2a95b59acbb417271f8f7f
Rachid OUHSSAINE <rachid1.ouhssaine@orange.com>
2020-04-18 11:52:27 +0200

    [dojo] solution exo 5

--- src/main/java/org/dojo/java8/exercise5/FileOperations.java
+++ src/main/java/org/dojo/java8/exercise5/FileOperations.java
@@ -4,76 +4,41 @@ import org.dojo.java8.model.Address;
 import org.dojo.java8.model.Role;
 import org.dojo.java8.model.User;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class FileOperations {
 
-    //TODO Replace By Files.lines, use static method reference
-    public static List<User> loadUsersFromCsv(Path csvPath) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(csvPath.toFile()))) {
-            String line;
-            boolean firstLine = true;
-            List<User> users = new ArrayList<>();
-            while ((line = reader.readLine()) != null) {
-                if (!firstLine) {
-                    users.add(lineToUser(line));
-                }
-                firstLine = false;
-            }
+ 	//TODO Replace By Files.lines, use static method reference
+	public static List<User> loadUsersFromCsv(Path csvPath) {
+		try (Stream<String> lines = Files.lines(csvPath)) {
+			return lines
+					.skip(1)
+					.map(FileOperations::lineToUser)
+					.collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Collections.emptyList();
+		}
+	}
 
-            return users;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
-    //TODO Replace by Files.walk and remove visitor. Use Optional.orElseThrow for throw FileNotFoundException
-    public static Path findRecursivelyFileByName(String path, String fileName) throws IOException {
-        Path rootDirectory = Paths.get(path);
-
-        SearchVisitor searchVisitor = new SearchVisitor(fileName);
-
-        Files.walkFileTree(rootDirectory, searchVisitor);
-        Path fileFound = searchVisitor.fileFound;
-        if (fileFound == null) {
-            throw new FileNotFoundException();
-        }
-        return fileFound;
-    }
-
-    public static class SearchVisitor extends SimpleFileVisitor<Path> {
-
-        private Path fileFound;
-
-        private String fileNameToSearch;
-
-        public SearchVisitor(String fileNameToSearch) {
-            this.fileNameToSearch = fileNameToSearch;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            if (attrs.isRegularFile() && file.getFileName().toString().equals(fileNameToSearch)) {
-                fileFound = file;
-                return FileVisitResult.TERMINATE;
-            }
-            return FileVisitResult.CONTINUE;
-        }
-
-    }
+	//TODO Replace by Files.walk and remove visitor. Use Optional.orElseThrow for throw FileNotFoundException
+	public static Path findRecursivelyFileByName(String path, String fileName) throws IOException {
+		try (Stream<Path> files = Files.walk(Paths.get(path), FileVisitOption.FOLLOW_LINKS)) {
+			return files
+					.filter(currentPath -> currentPath.getFileName().equals(Paths.get(fileName)))
+					.findFirst()
+					.orElseThrow(FileNotFoundException::new);
+		}
+	}
 
     private static User lineToUser(String line) {
         String[] columns = line.split(",");
